980a3e43e97d0417ae8ad00603abea1af4b93998
Facundo Arebalo <facuar1589@gmail.com>
2024-03-23 12:13:44 -0300

    correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Navegador from './components/nav/Navegador'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Rutas from './components/routes/Rutas'
 
 
 function App() {
   
 
   return (
+    <>
     <Navegador/>
+    {/* <Rutas/> */}
+    </>
   )
 }
 
