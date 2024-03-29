a8c165ab9deab6991b1b76284976b41a71e28c15
Facundo Arebalo <facuar1589@gmail.com>
2024-03-29 19:52:58 -0300

    correcion provider

--- src/context/serviceContext/ServiceContext.jsx
+++ src/context/serviceContext/ServiceContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const serviceContextProvider = createContext();
 
-const ServiceContext = () => {
+const ServiceContext = ({ children }) => {
   const [services, setServices] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
 
@@ -66,7 +66,10 @@ const upDateService = async (service) => {
         getService,
         upDateService,
         deleteService,
-      }}></serviceContextProvider.Provider>
+        setPageNumber,
+      }}>
+        {children}
+      </serviceContextProvider.Provider>
   );
 };
 
