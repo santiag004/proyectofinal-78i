b69890183cc01c41c36648e4c4ca81c7444c5953
Javier <javrodriguez2003@gmail.com>
2024-04-06 13:39:15 -0300

    card casi terminadas

---
+++ src/components/cards/Cards.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import "../../css/cards.css";
+
+const Cards = () => {
+  return (
+    <>
+      <Row>
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          className="py-4 d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Body className="cardBody">
+              <Card.Title className="cardBody  text-white d-flex justify-content-center">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Horario
+              </Card.Text>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Fecha
+              </Card.Text>
+              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
+                Profesora
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          className="py-4 d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Body className="cardBody">
+              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Horario
+              </Card.Text>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Fecha
+              </Card.Text>
+              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
+                Profesora
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          className="py-4 d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Body className="cardBody">
+              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Horario
+              </Card.Text>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Fecha
+              </Card.Text>
+              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
+                Profesora
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          className="py-4 d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Body className="cardBody">
+              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Horario
+              </Card.Text>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Fecha
+              </Card.Text>
+              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
+                Profesora
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          className="py-4 d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Body className="cardBody">
+              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Horario
+              </Card.Text>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Fecha
+              </Card.Text>
+              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
+                Profesora
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          className="py-4 d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Body className="cardBody">
+              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Horario
+              </Card.Text>
+              <Card.Text className="cardBody d-flex justify-content-center text-white">
+                Fecha
+              </Card.Text>
+              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
+                Profesora
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Cards;
---
+++ src/views/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Cards from '../../components/cards/Cards'
+
+const Home = () => {
+  return (
+    <Cards/>
+  )
+}
+
+export default Home
