bb7d331323c38a9634396865b1ad8f400b6c43de
Javier <javrodriguez2003@gmail.com>
2024-04-06 17:39:16 -0300

    Logos agregados

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import "../../css/cards.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarDays, faClock} from '@fortawesome/free-solid-svg-icons'
 
 const Cards = () => {
   return (
@@ -17,57 +19,11 @@ const Cards = () => {
           <Card style={{ width: "18rem" }}>
             <Card.Body className="cardBody">
               <Card.Title className="cardBody  text-white d-flex justify-content-center">Detalle de Clases</Card.Title>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Horario
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center" >
+              <FontAwesomeIcon icon={faClock} className="cardBody px-1"/> Horario
               </Card.Text>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Fecha
-              </Card.Text>
-              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
-                Profesora
-              </Card.Link>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col
-          xs={12}
-          sm={12}
-          md={6}
-          lg={6}
-          className="py-4 d-flex justify-content-center"
-        >
-          <Card style={{ width: "18rem" }}>
-            <Card.Body className="cardBody">
-              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Horario
-              </Card.Text>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Fecha
-              </Card.Text>
-              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
-                Profesora
-              </Card.Link>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col
-          xs={12}
-          sm={12}
-          md={6}
-          lg={6}
-          className="py-4 d-flex justify-content-center"
-        >
-          <Card style={{ width: "18rem" }}>
-            <Card.Body className="cardBody">
-              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Horario
-              </Card.Text>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Fecha
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center">
+              <FontAwesomeIcon icon={faCalendarDays} className="cardBody px-1"/>Fecha
               </Card.Text>
               <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
                 Profesora
@@ -84,12 +40,12 @@ const Cards = () => {
         >
           <Card style={{ width: "18rem" }}>
             <Card.Body className="cardBody">
-              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Horario
+              <Card.Title className="cardBody  text-white d-flex justify-content-center">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center" >
+              <FontAwesomeIcon icon={faClock} className="cardBody px-1"/> Horario
               </Card.Text>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Fecha
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center">
+              <FontAwesomeIcon icon={faCalendarDays} className="cardBody px-1"/>Fecha
               </Card.Text>
               <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
                 Profesora
@@ -108,12 +64,12 @@ const Cards = () => {
         >
           <Card style={{ width: "18rem" }}>
             <Card.Body className="cardBody">
-              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Horario
+              <Card.Title className="cardBody  text-white d-flex justify-content-center">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center" >
+              <FontAwesomeIcon icon={faClock} className="cardBody px-1"/> Horario
               </Card.Text>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Fecha
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center">
+              <FontAwesomeIcon icon={faCalendarDays} className="cardBody px-1"/>Fecha
               </Card.Text>
               <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
                 Profesora
@@ -130,12 +86,58 @@ const Cards = () => {
         >
           <Card style={{ width: "18rem" }}>
             <Card.Body className="cardBody">
-              <Card.Title className="cardBody d-flex justify-content-center text-white">Detalle de Clases</Card.Title>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Horario
+              <Card.Title className="cardBody  text-white d-flex justify-content-center">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center" >
+              <FontAwesomeIcon icon={faClock} className="cardBody px-1"/> Horario
               </Card.Text>
-              <Card.Text className="cardBody d-flex justify-content-center text-white">
-                Fecha
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center">
+              <FontAwesomeIcon icon={faCalendarDays} className="cardBody px-1"/>Fecha
+              </Card.Text>
+              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
+                Profesora
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          className="py-4 d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Body className="cardBody">
+              <Card.Title className="cardBody  text-white d-flex justify-content-center">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center" >
+              <FontAwesomeIcon icon={faClock} className="cardBody px-1"/> Horario
+              </Card.Text>
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center">
+              <FontAwesomeIcon icon={faCalendarDays} className="cardBody px-1"/>Fecha
+              </Card.Text>
+              <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
+                Profesora
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          className="py-4 d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Body className="cardBody">
+              <Card.Title className="cardBody  text-white d-flex justify-content-center">Detalle de Clases</Card.Title>
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center" >
+              <FontAwesomeIcon icon={faClock} className="cardBody px-1"/> Horario
+              </Card.Text>
+              <Card.Text className="cardBody d-flex justify-content-center text-white align-items-center">
+              <FontAwesomeIcon icon={faCalendarDays} className="cardBody px-1"/>Fecha
               </Card.Text>
               <Card.Link href="#" className="cardBody d-flex justify-content-center text-white text-decoration-none">
                 Profesora
